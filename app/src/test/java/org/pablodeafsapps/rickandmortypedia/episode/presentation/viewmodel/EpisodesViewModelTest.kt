2cb3e13d577a103779d414cc36c57e7f2d56d259
Pablo L. Sordo <pablodeafsapps@gmail.com>
2022-11-28 06:46:11 +0100

    feat: add 'EpisodesViewModel' behaviour tests

--- app/src/test/java/org/pablodeafsapps/rickandmortypedia/episode/presentation/viewmodel/EpisodesViewModelTest.kt
+++ app/src/test/java/org/pablodeafsapps/rickandmortypedia/episode/presentation/viewmodel/EpisodesViewModelTest.kt
@@ -1,27 +1,25 @@
-package org.pablodeafsapps.rickandmortypedia.episode.presentation.presenter
+package org.pablodeafsapps.rickandmortypedia.episode.presentation.viewmodel
 
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.*
 import org.junit.After
-import org.junit.Assert.*
+import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
 import org.mockito.Mockito
 import org.pablodeafsapps.rickandmortypedia.episode.domain.EpisodesDomainLayerContract
 import org.pablodeafsapps.rickandmortypedia.episode.domain.model.Episodes
-import org.pablodeafsapps.rickandmortypedia.episode.presentation.EpisodesContract
 
 @OptIn(ExperimentalCoroutinesApi::class)
-class EpisodesPresenterTest {
+class EpisodesViewModelTest {
 
-    private lateinit var sut: EpisodesPresenter
+    private lateinit var sut: EpisodesViewModel
     private lateinit var testScheduler: TestCoroutineScheduler
 
     @Before
     fun setUp() {
-        sut = EpisodesPresenter(
-            episodesView = Mockito.mock(EpisodesContract.View::class.java),
+        sut = EpisodesViewModel(
             getAllEpisodesUc = Mockito.mock(EpisodesDomainLayerContract.PresentationLayer.UseCase::class.java)
         )
         testScheduler = TestCoroutineScheduler()
@@ -35,25 +33,25 @@ class EpisodesPresenterTest {
 
     @OptIn(ExperimentalCoroutinesApi::class)
     @Test
-    fun `When view gets created and the usecase response is successful, 'loadEpisodes' is invoked`() = runTest {
+    fun `When view gets created and the usecase response is successful, 'episodes' property is not 'null'`() = runTest {
         // given
         Mockito.`when`(sut.getAllEpisodesUc.getAllEpisodes()).thenReturn(getDummyEpisodesEmptyResult())
         // when
         sut.onViewCreated()
         advanceUntilIdle()
         // then
-        Mockito.verify(sut.episodesView).loadEpisodes(getDummyEpisodesEmpty())
+        Assert.assertTrue(sut.episodes.value != null)
     }
 
     @Test
-    fun `When view gets created and the usecase response fails, 'showMessage' is invoked`() = runTest {
+    fun `When view gets created and the usecase response fails, 'episodes' property remains 'null'`() = runTest {
         // given
         Mockito.`when`(sut.getAllEpisodesUc.getAllEpisodes()).thenReturn(getDummyFailureResult())
         // when
         sut.onViewCreated()
         advanceUntilIdle()
         // then
-        Mockito.verify(sut.episodesView).showMessage(Mockito.anyString())
+        Assert.assertTrue(sut.episodes.value == null)
     }
 
     private fun getDummyEpisodesEmptyResult() : Result<Episodes> =
